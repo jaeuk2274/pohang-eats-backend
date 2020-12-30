51bf41e452fa89af99be5e1ff1de346939c6cb80
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-30 21:51:28 +0900

    InputTypes and ArgumentTypes

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,19 @@
-import { Resolver, Query } from "@nestjs/graphql";
+import { Resolver, Query, Args, Mutation } from "@nestjs/graphql";
+import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
+import { Restaurant } from "./entities/restaurant.entity";
 
-@Resolver()
+// @Resolver()
+@Resolver(of => Restaurant) // 안써도 되나 코드가 더 직관적
 export class RestaurantResolver {
+    @Query(returns => [Restaurant])  
+    restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+        console.log(veganOnly);
+        return [];
+    }
 
-    @Query(returns => Boolean)  // @Query(()) => Boolean) 
-    isPizzaGood(): Boolean { // isPizzaGood()
-        return true;
+    @Mutation(returns => Boolean)  
+    createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+      console.log(createRestaurantDto);
+      return true;
     }
 } 
