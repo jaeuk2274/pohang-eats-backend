a1c2311441368676b9e77dffb56b85f2cc03d599
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-31 21:02:59 +0900

    update restaurant

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -27,8 +27,13 @@ export class RestaurantResolver {
     } 
  
     @Mutation(returns => Boolean) 
-    async updateRestaurant(@Args('input') updateRestaurantDto: UpdateRestaurantDto,) {
-      console.log(updateRestaurantDto);
-      return true;
+    async updateRestaurant(@Args('input') updateRestaurantDto: UpdateRestaurantDto,): Promise<boolean> {
+      try{
+          await this.restaurantService.updateRestaurant(updateRestaurantDto);
+          return true;
+      }catch(e){
+          console.log(e);
+          return false;
+      }
     }
 }
