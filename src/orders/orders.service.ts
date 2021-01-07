5d77900248f0856ca1356c7e8ec5a7a7eee94f4a
jaeuk2274 <jaeuk2274@gmail.com>
2021-01-07 21:02:58 +0900

    create order

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { async } from 'rxjs';
+import { Dish } from 'src/restaurants/entities/dish.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 
 @Injectable()
@@ -14,6 +16,10 @@ export class OrderService {
     private readonly orders: Repository<Order>,
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
+    @InjectRepository(OrderItem)
+    private readonly orderItems: Repository<OrderItem>,
+    @InjectRepository(Dish)
+    private readonly dishes: Repository<Dish>,
   ) {}
 
   async crateOrder(
@@ -27,12 +33,43 @@ export class OrderService {
         error: 'Restaurant not found',
       };
     }
-    const order = await this.orders.save(
-      this.orders.create({
-        customer,
-        restaurant,
-      }),
-    );
-    console.log(order);
+
+    for (const item of items) {
+      const dish = await this.dishes.findOne(item.dishId);
+      if (!dish) {
+        return {
+          ok: false,
+          error: 'Dish not found.',
+        };
+      }
+      console.log(`Dish price: ${dish.price}`);
+      for (const itemOption of item.options) {
+        const dishOption = dish.options.find(
+          (dishOption) => dishOption.name === itemOption.name,
+        );
+        if (dishOption) {
+          if (dishOption.extra) {
+            console.log(`$USD + ${dishOption.extra}`);
+          } else {
+            const dishOptionChoice = dishOption.choices.find(
+              (optionChoice) => optionChoice.name === itemOption.choice,
+            );
+            if (dishOptionChoice) {
+              if (dishOptionChoice.extra) {
+                console.log(`$USD + ${dishOptionChoice.extra}`);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // const order = await this.orders.save(
+    //   this.orders.create({
+    //     customer,
+    //     restaurant,
+    //   }),
+    // );
+    // console.log(order);
   }
 }
